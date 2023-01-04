c642827f52e8f08f90d37b397bf8e1d236ef9e27
eliabev <eliabev@gmail.com>
2023-01-04 20:15:55 -0300

    install esLint and perform first test

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const valorHorasExtras = (salario, horasExtras) => salario + horasExtras;
+const somaHorasExtras = (salario, horasExtras) => salario + horasExtras;
+const calculoDeDescontos = (salario, descontos) => salario - descontos;
+
+const teste = (titulo, esperado, retornado) => {
+  if (esperado === retornado) {
+    console.log(`titulo ${titulo} passou!`);
+  } else {
+    console.error(`titulo ${titulo} não passou`);
+  }
+};
+
+teste('somaHorasExtras', 2500, somaHorasExtras(2000, 500));
+teste('calculoDeDescontos', 2200, calculoDeDescontos(2200, 300));
